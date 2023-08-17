5fa59cad4e75bad7aac27a2ddbd2657894d18f17
HoangAnh <hoanganh51199@gmail.com>
2023-08-17 23:44:01 +0700

    lan1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import BaiTapLayout2 from './components/BaiTapLayout2/BaiTapLayout2';
+import BaiTapShoe from './components/BaiTapShoe/BaiTapShoe';
+
+function App() {
+  return (
+   <>
+   <BaiTapShoe/>
+   </>
+  );
+}
+
+export default App;
